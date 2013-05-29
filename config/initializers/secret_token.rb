24e6aa833a4db98ed319474b0ff30dab7409d64c
erindrummond <e.b.drummond@gmail.com>
2013-05-29 12:43:48 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appointime::Application.config.secret_token = 'aa6b4ad5671cf26342198dbce10e4c8622cd8c6bf8ca59e7504f8f8b19ac2f778f591c22214a230f50e03d1199b2130cbc4c82554007dd75b2a1a6866129786f'
