077e6f59afde34bb8bbee92a8dbdd57c9b9e48d8
erindrummond <e.b.drummond@gmail.com>
2013-05-30 14:49:44 -0600

    time slot tests passing

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,13 +1,18 @@
 class TimeSlot
-  attr_reader :starts, :duration
+  attr_reader :starts, :ends, :duration
 
   def initialize(params)
-    @starts = params[:starts]
-    @duration = params[:duration]
-  end
-
-  def ends
-    self.starts + (duration * 60)
+    @starts = params.fetch(:starts) {Time.new(2013, 1, 1, 8)}
+    if params[:duration]
+      @duration = params[:duration]
+      @ends = starts + (duration * 60)
+    elsif params[:ends]
+      @ends = params[:ends]
+      @duration = (ends - starts)/60
+    else
+      @ends = Time.new(2013, 1, 1, 18)
+      @duration = (ends - starts)/60
+    end
   end
 
 
--- spec/models/time_slot_spec.rb
+++ spec/models/time_slot_spec.rb
@@ -2,12 +2,45 @@ require './app/models/time_slot'
 
 describe TimeSlot do
   let(:appt){ stub(start_time: Time.new(2013, 1, 1, 15), duration: 90) }
-  let(:appt_time_slot){ TimeSlot.new(starts: appt.start_time, duration: appt.duration) }
 
-  it "has an end time" do
-    expect(appt_time_slot.ends).to eq(Time.new(2013, 1, 1, 16, 30))
+  context "an appointment time slot" do
+    let(:time_slot){ TimeSlot.new(starts: appt.start_time, duration: appt.duration) }
+
+    it "has an end time" do
+      expect(time_slot.ends).to eq(Time.new(2013, 1, 1, 16, 30))
+    end
   end
 
-  it ""
+  context "an empty time slot at the beginning of the day" do
+    let(:time_slot){ TimeSlot.new(ends: appt.start_time) }
 
+    it "starts at 8am" do
+      expect(time_slot.starts).to eq(Time.new(2013, 1, 1, 8))
+    end
+
+    it "ends at 3pm" do
+      expect(time_slot.ends).to eq(appt.start_time)
+    end
+
+    it "lasts for 420 minutes" do
+      expect(time_slot.duration).to eq(420)
+    end
+  end
+
+  context "an empty slot at the end of the day" do
+    let(:start_time){ Time.new(2013, 1, 1, 16, 30) }
+    let(:time_slot){ TimeSlot.new(starts: start_time) }
+
+    it "starts at 4:30pm" do
+      expect(time_slot.starts).to eq(start_time)
+    end
+
+    it "ends at 6:00pm" do
+      expect(time_slot.ends).to eq(Time.new(2013, 1, 1, 18))
+    end
+
+    it "lasts for 90 minutes" do
+      expect(time_slot.duration).to eq(90)
+    end
+  end
 end
