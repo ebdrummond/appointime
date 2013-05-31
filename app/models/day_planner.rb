380ed1894ab872b3a43d08a6dc0af412de2b8792
erindrummond <e.b.drummond@gmail.com>
2013-05-30 22:13:43 -0600

    two out of five day planner scenario specs passing, and that ain't bad

--- app/models/day_planner.rb
+++ app/models/day_planner.rb
@@ -5,7 +5,7 @@ class DayPlanner
   attr_reader :appointments
 
   def initialize(appointments)
-    @appointments = appointments#.sort_by{|appt| appt.start_time}
+    @appointments = appointments.sort_by{|appt| appt.start_time}
   end
 
   def time_slots
@@ -13,15 +13,56 @@ class DayPlanner
     appointments.each do |appt|
       slots << TimeSlot.new(starts: appt.start_time, duration: appt.duration)
     end
-    slots << morning_slot
-    slots.concat(in_between_slots)
+    if !appointments.first.start_time.to_s.include?(Clock.new(8).to_s)
+      slots << morning_slot
+    end
     slots << afternoon_slot
-    slots
+    slots.sort_by{|slot| slot.starts.time}
   end
 
+  def morning_slot
+    TimeSlot.new(ends: appointments.first.start_time)
+  end
+
+  def afternoon_slot
+    TimeSlot.new(starts: appointments.last.start_time + appointments.last.duration * 60)
+  end
+
+  def appointment_start_times
+    appointments.collect{|appt| Clock.new(appt.start_time.hour, appt.start_time.min).to_s}
+  end
+
+  def time_slot_start_times
+    time_slots.collect{|ts| ts.starts.to_s}
+  end
+
+  def open_slots
+    time_slots.select{|ts| (time_slot_start_times - appointment_start_times).include?(ts.starts.to_s)}
+  end
+
+    # unless appointments.first.start_time.to_s.include?(Clock.new(8).to_s)
+
+
+    # slots = []
+    # appointments.each do |appt|
+    #   slots << TimeSlot.new(starts: appt.start_time, duration: appt.duration)
+    # end
+    # slots << morning_slot
+    # slots.concat(in_between_slots)
+    # slots << afternoon_slot
+    # slots
 
 
 
+  # def in_between_slots
+  #   slots = []
+  #   appointments.each_with_index do |appt, index|
+  #     break if index == appointments.size - 1
+  #     next_appt = appointments[index+1]
+  #     slots << TimeSlot.new(starts: appt.start_time + appt.duration * 60, ends: next_appt.start_time)
+  #   end
+  #   slots
+  # end
 
   # def initialize(appointment)
   #   @appointment = appointment
--- spec/models/day_planner_spec.rb
+++ spec/models/day_planner_spec.rb
@@ -3,25 +3,136 @@ require './app/models/time_slot'
 require './app/models/clock'
 
 describe DayPlanner do
-  let(:appt1){ stub(start_time: Time.new(2013, 1, 1, 10, 30), duration: 90) }
-  # let(:appt2){ stub(start_time: Time.new(2013, 1, 1, 15), duration: 90) }
-  # let(:appts){ [appt1, appt2] }
-  let(:day_planner){ DayPlanner.new(appt1) }
+  it "sorts the appointments by start time" do
+    appt1 = stub(start_time: Time.new(2013, 1, 1, 15), duration: 90)
+    appt2 = stub(start_time: Time.new(2013, 1, 1, 8), duration: 90)
+    appts = [appt1, appt2]
 
-  context "with a single appointment" do
+    expect(DayPlanner.new(appts).appointments).to eq([appt2, appt1])
+  end
+
+  context "with a single appointment in the middle of the day" do
+    let(:appt1){ stub(start_time: Time.new(2013, 1, 1, 10, 30), duration: 90) }
+    let(:appts){ [appt1] }
+    let(:day_planner){ DayPlanner.new(appts) }
+
     it "takes in appointments" do
       expect(day_planner.appointments).to eq(appts)
     end
 
-    it "knows the start time and duration of the appointmentss" do
-      expect(day_planner.appointments.last.start_time).to eq(Time.new(2013, 1, 1, 15))
-      expect(day_planner.appointments.last.duration).to eq(90)
+    it "knows the start time and duration of the appointments" do
+      expect(day_planner.appointments.first.start_time).to eq(Time.new(2013, 1, 1, 10, 30))
+      expect(day_planner.appointments.first.duration).to eq(90)
     end
 
-    it "has time slots" do
-      expect(day_planner.time_slots.count).to eq(5)
+    it "has three time slots" do
+      expect(day_planner.time_slots.count).to eq(3)
+    end
+
+    it "sorts the time slots by start time" do
+      expect(day_planner.time_slots.first.starts).to eq(Clock.new(8))
+    end
+
+    it "has start times for the time slots" do
+      expect(day_planner.time_slots[1].starts).to eq(Clock.new(10, 30))
+    end
+
+    it "has end times for the time slots" do
+      expect(day_planner.time_slots[1].ends).to eq(Clock.new(12))
+    end
+
+    it "has durations for the time slots" do
+      expect(day_planner.time_slots[1].duration).to eq(90)
+    end
+
+    it "identifies open time slots" do
+      expect(day_planner.open_slots.count).to eq(2)
     end
   end
+
+  context "with a single appointment at the beginning of the day" do
+    let(:appt1){ stub(start_time: Time.new(2013, 1, 1, 8), duration: 90) }
+    let(:appts){ [appt1] }
+    let(:day_planner){ DayPlanner.new(appts) }
+
+    it "takes in appointments" do
+      expect(day_planner.appointments).to eq(appts)
+    end
+
+    it "knows the start time and duration of the appointments" do
+      expect(day_planner.appointments.first.start_time).to eq(Time.new(2013, 1, 1, 8))
+      expect(day_planner.appointments.first.duration).to eq(90)
+    end
+
+    it "has three time slots" do
+      expect(day_planner.time_slots.count).to eq(2)
+    end
+
+    it "sorts the time slots by start time" do
+      expect(day_planner.time_slots.first.starts).to eq(Clock.new(8))
+    end
+
+    it "has start times for the time slots" do
+      expect(day_planner.time_slots[1].starts).to eq(Clock.new(9, 30))
+    end
+
+    it "has end times for the time slots" do
+      expect(day_planner.time_slots[1].ends).to eq(Clock.new(18))
+    end
+
+    it "has durations for the time slots" do
+      expect(day_planner.time_slots[1].duration).to eq(510)
+    end
+
+    it "identifies open time slots" do
+      expect(day_planner.open_slots.count).to eq(1)
+    end
+  end
+
+  # context "with a single appointment at the beginning of the day" do
+  #   let(:appt1){ stub(start_time: Time.new(2013, 1, 1, 8), duration: 90) }
+  #   let(:appts){ [appt1] }
+  #   let(:day_planner){ DayPlanner.new(appts) }
+
+  #   it "takes in appointments" do
+  #     expect(day_planner.appointments).to eq(appts)
+  #   end
+
+  #   it "knows the start time and duration of the appointments" do
+  #     expect(day_planner.appointments.first.start_time).to eq(Time.new(2013, 1, 1, 8))
+  #     expect(day_planner.appointments.first.duration).to eq(90)
+  #   end
+
+  #   it "has three time slots" do
+  #     expect(day_planner.time_slots.count).to eq(2)
+  #   end
+
+  #   it "sorts the time slots by start time" do
+  #     expect(day_planner.time_slots.first.starts).to eq(Clock.new(8))
+  #   end
+
+  #   it "has start times for the time slots" do
+  #     expect(day_planner.time_slots[1].starts).to eq(Clock.new(9, 30))
+  #   end
+
+  #   it "has end times for the time slots" do
+  #     expect(day_planner.time_slots[1].ends).to eq(Clock.new(18))
+  #   end
+
+  #   it "has durations for the time slots" do
+  #     expect(day_planner.time_slots[1].duration).to eq(510)
+  #   end
+
+  #   it "identifies open time slots" do
+  #     expect(day_planner.open_slots.count).to eq(1)
+  #   end
+  # end
+
+
+
+
+  # let(:appt2){ stub(start_time: Time.new(2013, 1, 1, 15), duration: 90) }
+  # let(:appts){ [appt1, appt2] }
 
   # it "has start times for the time slots" do
   #   time_slot = DayPlanner.new(appt).time_slots.first
